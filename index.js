1a58785515e8c66a8c63c940dca270b678581091
elamahpla <theamazingness@gmail.com>
2018-09-05 18:23:19 -0500

    shell game

---
+++ index.js
@@ -0,0 +1,21 @@
+// const cards = document.querySelectorAll('.card');
+// for (let i = 0; i < cards.length; i++) {
+//   const target = Math.floor(Math.random() * cards.length - 1) + 1;
+//   const targetTwo = Math.floor(Math.random() * cards.length - 1) + 1;
+//   cards.index(target).before(cards.index(targetTwo));
+// }
+
+// const load = () => {
+//   const cards = document.querySelectorAll('.card');
+//   const cardContainer = document.getElementById('cardContainer').clientHeight;
+// }
+// window.onload = load;
+
+// Does not work w/ CSS Grid
+$(function () {
+  var parent = $(".cards");
+  var divs = parent.children();
+  while (divs.length) {
+    parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+  }
+});
