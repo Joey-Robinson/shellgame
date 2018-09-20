70bd10f446ef415decc0fbd6a3a6b2e550d10bcc
elamahpla <theamazingness@gmail.com>
2018-09-20 18:28:49 -0500

    :turtle:

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const cardsArray = [{
+    'name': 'turtleone',
+    'img': '../images/trutleone.jpg',
+  },
+  {
+    'name': 'turtletwo',
+    'img': '../images/trutletwo.jpg',
+  },
+  {
+    'name': 'turtlethree',
+    'img': '../images/trutlethree.jpg',
+  },
+  {
+    'name': 'turtlefour',
+    'img': '../images/trutlefour.jpg',
+  },
+  {
+    'name': 'turtlefive',
+    'img': '../images/trutlefive.jpg',
+  },
+  {
+    'name': 'turtlesix',
+    'img': '../images/trutlesix.jpg',
+  },
+  {
+    'name': 'turtleseven',
+    'img': '../images/trutleseven.jpg',
+  },
+  {
+    'name': 'turtleeight',
+    'img': '../images/trutleeight.jpg',
+  },
+  {
+    'name': 'turtlenine',
+    'img': '../images/trutlenine.jpg',
+  },
+  {
+    'name': 'turtleten',
+    'img': '../images/trutleten.jpg',
+  },
+  {
+    'name': 'turtleeleven',
+    'img': '../images/trutleeleven.jpg',
+  },
+  {
+    'name': 'turtletwelve',
+    'img': '../images/trutletwelve.jpg',
+  },
+];
