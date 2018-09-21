f7a635c8e931bc46457807c8be950f68454b6339
elamahpla <theamazingness@gmail.com>
2018-09-21 17:33:42 -0500

    Removed dist, removed scss assets and just keeping raw css.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-const cardsArray = [
-  {
+const cardsArray = [{
     name: "turtleone",
     img: "../images/turtleone.jpg"
   },
@@ -48,37 +47,40 @@ const cardsArray = [
     img: "../images/turtletwelve.jpg"
   }
 ];
+const moreTurtles = cardsArray.concat(cardsArray).sort(() => 0.5 - Math.random());
 
-const shellGame = document.getElementById("shellGame");
-const layout = document.createElement("section");
+let guessOne = "";
+let guessTwo = "";
+let count = 0;
+let previousTurtle = null;
+let pause = 1200;
+
+const shellGame = document.getElementById("shellGame"); // Game
+const layout = document.createElement("section"); // Grid
 layout.setAttribute("class", "primary");
 shellGame.appendChild(layout);
 
-let moreTurtles = cardsArray.concat(cardsArray);
-moreTurtles.sort(() => 0.5 - Math.random());
-
 moreTurtles.forEach(item => {
+  const {
+    name,
+    img
+  } = item;
+
   const turtleFront = document.createElement("div");
   turtleFront.classList.add("turtleFront");
 
   const turtleBack = document.createElement("div");
   turtleBack.classList.add("turtleBack");
-  turtleBack.style.backgroundImage = `url(${item.img})`;
+  turtleBack.style.backgroundImage = `url(${img})`;
 
   const card = document.createElement("div");
   card.classList.add("turtles");
-  card.dataset.name = item.name;
+  card.dataset.name = name;
 
   layout.appendChild(card);
   card.appendChild(turtleFront);
   card.appendChild(turtleBack);
 });
-
-let pause = 1200;
-let count = 0;
-let guessOne = "";
-let guessTwo = "";
-let previousTurtle = null;
 
 const matchTurtle = () => {
   const selected = document.querySelectorAll(".clickedturtle");
@@ -91,22 +93,23 @@ const resetTurtle = () => {
   guessOne = "";
   guessTwo = "";
   count = 0;
+  previousTurtle = null;
   const selectedTurtle = document.querySelectorAll(".clickedturtle");
   selectedTurtle.forEach(turtleCard => {
     turtleCard.classList.remove("clickedturtle");
   });
 };
 
-shellGame.addEventListener("click", event => {
-  let turtleClick = event.target;
+layout.addEventListener("click", event => {
+  const turtleClick = event.target;
   if (
     turtleClick.nodeName === "SECTION" ||
     turtleClick === previousTurtle ||
-    turtleClick.parentNode.classList.contains("clickedturtle")
+    turtleClick.parentNode.classList.contains("clickedturtle") ||
+    turtleClick.parentNode.classList.contains('matchingturtles')
   ) {
     return;
   }
-  turtleClick.classList.add("clickedturtle");
   if (count < 2) {
     count++;
     if (count === 1) {
@@ -118,13 +121,11 @@ shellGame.addEventListener("click", event => {
       console.log(guessTwo);
       turtleClick.parentNode.classList.add("clickedturtle");
     }
-    if (guessOne !== "" && guessTwo !== "") {
+    if (guessOne && guessTwo) {
       if (guessOne === guessTwo) {
         setTimeout(matchTurtle, pause);
-        setTimeout(resetTurtle, pause);
-      } else {
-        setTimeout(resetTurtle, pause);
       }
+      setTimeout(resetTurtle, pause);
     }
     previousTurtle = turtleClick;
   }
